1e6aa4d259dca0553845433c483a85e7b3557094
PavKur906 <120056148+PavKur906@users.noreply.github.com>
2024-11-06 22:10:27 +0300

    Add files via upload

--- transfer_toDB.php
+++ transfer_toDB.php
@@ -1,13 +1,14 @@
 <?php
 if (isset($_POST["username"]) && isset($_POST["usertel"]) && isset($_POST["usermail"])) {
       
-    $conn = new mysqli("sql7.freemysqlhosting.net", "sql7743051", "Wza8cSFwbM", "clients_info");
+    $conn = new mysqli("sql7.freemysqlhosting.net", "sql7743051", "Wza8cSFwbM", "sql7743051");
     if($conn->connect_error){
         die("Ошибка: " . $conn->connect_error);
     }
     $name = $conn->real_escape_string($_POST["username"]);
-    $age = $conn->real_escape_string($_POST["userage"]);
-    $sql = "INSERT INTO Users (name, age) VALUES ('$name', $age)";
+    $tel = $conn->real_escape_string($_POST["usertel"]);
+    $mail = $conn->real_escape_string($_POST["usermail"]);
+    $sql = "INSERT INTO clients_info (name, phone, mail) VALUES ('$name', '$tel', '$mail')";
     if($conn->query($sql)){
         echo "Ваши данные приняты!";
     } else{
